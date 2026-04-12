b2d061119c4748b2741a81c674ee0dd22bf8acf0
sam <sperez207@gmail.com>
2026-04-11 20:04:30 -0400

    Finished the charts tab

--- tabs/charts.py
+++ tabs/charts.py
@@ -3,13 +3,34 @@ import pandas as pd
 import plotly.express as px
 
 def render(df: pd.DataFrame):
-    numericDf = df.select_dtypes(include=['number'])
-    numericColumns = numericDf.columns.tolist()
+    numericCols = df.select_dtypes(include=['number']).columns
 
     st.subheader("Distributions")
-    distSelection = st.selectbox("Select a Feature to View its Distribution", 
-                                 options=numericColumns, 
-                                 index=list(numericColumns).index('price'))
-    nbins = min(df[distSelection].nunique(), 50)
-    distPlot = px.histogram(df, x=distSelection, nbins=nbins)
-    st.plotly_chart(distPlot)
+    with st.container(border=True):
+        distSelection = st.selectbox("Select a Feature to View its Distribution", 
+                                    options=numericCols, 
+                                    index=list(numericCols).index('price'),
+                                    key="dist-selectbox")
+        nbins = min(df[distSelection].nunique(), 50)
+        distPlot = px.histogram(df, x=distSelection, nbins=nbins)
+        st.plotly_chart(distPlot)
+
+    st.subheader("Box Plots")
+    with st.container(border=True):
+        boxSelection = st.selectbox("Select a Feature to View its Distribution", 
+                                    options=numericCols,
+                                    index=list(numericCols).index('price'),
+                                    key="box-selectbox")
+        boxPlot = px.box(df, x=boxSelection)
+        st.plotly_chart(boxPlot)
+
+    nonNumericCols = df.select_dtypes(exclude='number').columns
+    st.subheader("Categorical Features Frequency Plots")
+    with st.container(border=True):
+        catSelection = st.selectbox("Select a Feature to View its Distribution", 
+                                    options=nonNumericCols,
+                                    key="cat-selectbox")
+        catPlot = px.bar(df[catSelection].value_counts().reset_index(), x=catSelection, y='count')
+        barWidth = min(0.8, 0.2 * df[catSelection].nunique()) # dynamically calculate a good bar width depending on selected col
+        catPlot.update_traces(width=barWidth)
+        st.plotly_chart(catPlot)
