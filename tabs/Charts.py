96325db7359b2acac92b84e26e5e6afbe6977134
sam <sperez207@gmail.com>
2026-04-14 14:50:40 -0400

    Added the What Drives Price? tab

--- tabs/Charts.py
+++ tabs/Charts.py
@@ -35,36 +35,17 @@ def render(df: pd.DataFrame):
             st.plotly_chart(distPlot, key="dist-plot")
 
     with priceTab:
-        priceRoomCol, priceNeighborhoodCol = st.columns([1, 1])
+        catCol, contCol = st.columns([1, 1])
 
-        # with priceRoomCol.container(border=True):
-        #     avgPriceByRoom = df.groupby('room_type')['price'].mean().reset_index(name='avg_price')
-        #     avgPriceByRoom = avgPriceByRoom.sort_values('avg_price', ascending=True)
-        #     roomPricePlot = px.bar(
-        #         avgPriceByRoom,
-        #         x = 'room_type',
-        #         y = 'avg_price',
-        #         labels={'room_type': 'Room Type', 'avg_price': 'Average Price ($)'}
-        #     )
-        #     roomPricePlot.update_traces(texttemplate='$%{y:.2f}', textposition='outside')
-        #     st.plotly_chart(roomPricePlot, use_container_width=True)
+        with catCol.container(border=True, height=760):
+            # show the 3 most important ones next, then just append the rest programatically
+            categoricalCols = ['room_type', 'private_bathroom']
+            for c in df.select_dtypes(include=['object', 'bool']).columns.tolist():
+                if c not in categoricalCols and c != 'neighbourhood_cleansed': # separate plot for neighbourhood
+                    categoricalCols.append(c)
 
-        # with priceNeighborhoodCol.container(border=True):
-        #     avgPriceByNeighborhood = df.groupby('neighbourhood_cleansed')['price'].mean().reset_index(name='avg_price')
-        #     avgPriceByNeighborhood = avgPriceByNeighborhood.sort_values('avg_price', ascending=True)
-        #     neighborhoodPricePlot = px.bar(
-        #         avgPriceByNeighborhood,
-        #         x = 'neighbourhood_cleansed',
-        #         y = 'avg_price',
-        #         labels={'neighbourhood_cleansed': 'Neighbourhood', 'avg_price': 'Average Price ($)'}
-        #     )
-        #     neighborhoodPricePlot.update_traces(texttemplate='$%{x:.2f}', textposition='outside')
-        #     st.plotly_chart(neighborhoodPricePlot, use_container_width=True)
-
-        with st.container(border=True):
-            categoricalCols = ['room_type', 'private_bathroom', 'neighbourhood_cleansed']
             catSelection = st.selectbox(
-                "Select a Categorical Feature to Plot Against Price",
+                "Select a Categorical Feature",
                 options=categoricalCols,
                 key="price-cat-selectbox"
             )
@@ -72,9 +53,65 @@ def render(df: pd.DataFrame):
             avgPriceByCat = avgPriceByCat.sort_values('avg_price', ascending=True)
             catPricePlot = px.bar(
                 avgPriceByCat,
+                title= f"Average price per {catSelection}",
                 x=catSelection,
                 y='avg_price',
                 labels={catSelection: catSelection.replace('_', ' ').title(), 'avg_price': 'Average Price ($)'}
             )
             catPricePlot.update_traces(texttemplate='$%{y:.2f}', textposition='outside')
             st.plotly_chart(catPricePlot, use_container_width=True)
+
+            st.markdown('---')
+            st.markdown("* **room_type** and **private_bathroom** appear to be great indicators of price, " \
+            "which makes sense, since both are features indicative of available space and privacy.")
+
+        with contCol.container(border=True):
+            continuousCols = ['accommodates', 'bedrooms', 'beds', 'bathrooms']
+
+            for c in df.select_dtypes(include=['number']).columns.tolist():
+                if c not in continuousCols and c not in excludeCols and c != 'price':
+                    continuousCols.append(c)
+
+            contSelection = st.selectbox("Select a Continuous Feature to Plot Against Price",
+                options=continuousCols,
+                key="price-cont-selectbox"
+            )
+            scatterPlot = px.scatter(
+                df,
+                title=f"{contSelection} vs price",
+                x=contSelection,
+                y='price',
+                labels={contSelection: contSelection.replace('_', ' ').title(), 'price': 'Price ($)'},
+                opacity=0.5
+            )
+            st.plotly_chart(scatterPlot, use_container_width=True)
+
+            st.markdown('---')
+            st.markdown("""
+                        * **accomodates**, **bedrooms**, **beds**, and **bathrooms** all show a positive correlation with price
+                        * **minimum_nights** shows a negative correlation with price
+                        * **estimated_occupancy** shows a slight negative correlation with price, with most highly occupied properties
+                          clustering in a lower price range
+                        """)
+
+        with st.container(border=True):
+            avgPriceByNeighborhood = df.groupby('neighbourhood_cleansed')['price'].mean().reset_index(name='avg_price')
+            avgPriceByNeighborhood = avgPriceByNeighborhood.sort_values('avg_price', ascending=True)
+            neighborhoodPricePlot = px.bar(
+                avgPriceByNeighborhood,
+                title='Average Price by Neighborhood',
+                x = 'neighbourhood_cleansed',
+                y = 'avg_price',
+                labels={'neighbourhood_cleansed': 'neighborhood', 'avg_price': 'Average Price ($)'},
+                height=500
+            )
+            neighborhoodPricePlot.update_traces(texttemplate='$%{y:.2f}', textposition='outside')
+            st.plotly_chart(neighborhoodPricePlot, use_container_width=True)
+
+            st.markdown('---')
+            st.markdown("""
+                        While many neighborhoods cluster in a similar price range, there is a meaningful difference from 
+                        \\$65 to \\$293 across the county. neighborhoods at the extremes (such as Pembroke Park and Plantation) 
+                        show very distinct averages that could prove useful for a ML model. However, the predictive power 
+                        of neighborhood may be limited for the majority of mid-range areas.
+                        """)
